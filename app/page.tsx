63e41e58e1860bed6c2271af2c477b46dc5f326a
hamza <essh4014@gmail.com>
2023-11-10 07:26:11 +0100

    testing the website

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Navbar from '@/components/Navbar/Navbar'
 import Hero from '@/components/hero/Hero'
 import { styles } from '@/styles/styles'
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className={`px-[30px] lg:px-[100px] w-full overflow-hidden`}>
+    <main className={`${styles.paddingx} w-full overflow-hidden`}>
     <Navbar />
     <Hero />
     </main>
